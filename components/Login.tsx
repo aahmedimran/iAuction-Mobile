3c496e074598c2dfa20dc839cca31f348c2017e5
aahmedimran <ahmed.eng2709@gmail.com>
2023-09-10 01:35:15 -0700

    create login screen

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,18 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { Dispatch, SetStateAction } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, {Dispatch, SetStateAction} from 'react';
+import {
+  moderateScale,
+  moderateVerticalScale,
+  verticalScale,
+} from 'react-native-size-matters';
+import {Fontfamily} from '../theme/fontFamily';
+import {themeColors} from '../theme/colors';
+import { size } from '../theme/fontstyle';
+import Button from '../common/Button';
 
 type LoginProps = {
-  setAlreadyUser?: Dispatch<SetStateAction<boolean>>,
-}
+  setAlreadyUser?: Dispatch<SetStateAction<boolean>>;
+};
 
-const Login: React.FC<LoginProps> = ({ setAlreadyUser }) => {
+const Login: React.FC<LoginProps> = ({setAlreadyUser}) => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text>Login</Text>
+      </View>
+      <View>
+        <TextInput
+          placeholder="Enter email"
+          editable
+          // multiline
+          // numberOfLines={4}
+          // maxLength={40}
+          // onChangeText={text => onChangeText(text)}
+          // value={value}
+          style={styles.TextInput}
+        />
+        <TextInput
+          placeholder="Password"
+          editable
+          // multiline
+          // numberOfLines={4}
+          // maxLength={40}
+          // onChangeText={text => onChangeText(text)}
+          // value={value}
+          style={styles.TextInput}
+        />
+      </View>
+      <Button title ='Login' containerStyle={styles.containerStyle}/>
+    
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  textContainer: {
+    alignItems: 'center',
+    marginVertical: verticalScale(20),
+  },
+  TextInput: {
+    padding: 10,
+    borderBottomWidth: 2,
+    marginHorizontal: moderateScale(20),
+    color: themeColors.aquaColor,
+  },
+  containerStyle:{
+    marginTop:40
+  }
+});
--- screen/Auth.tsx
+++ screen/Auth.tsx
@@ -5,17 +5,15 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Login from '../components/Login';
 import Signup from '../components/Signup';
 import Applogo from '../assets/svg/Applogo';
+import { Fontfamily } from '../theme/fontFamily';
+import { size } from '../theme/fontstyle';
 
 const Auth = () => {
-    const [alreadyUser, setAlreadyUser] = useState<boolean>(false)
+    const [alreadyUser, setAlreadyUser] = useState<boolean>(true)
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar backgroundColor={themeColors.aquaColor} />
-
-            <View style={{}}>
-            <Applogo/>
-<Text>vkf efm</Text>
-            </View>
+            <View style={{ alignItems: "center", marginTop: 70 }}><Text style={styles.heading}>I Auction</Text></View>
             {alreadyUser ? <Login setAlreadyUser={setAlreadyUser} /> : <Signup setAlreadyUser={setAlreadyUser} />}
 
 
@@ -29,5 +27,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: themeColors.aquaColor
+    },
+    heading: {
+        color: "white",
+        fontFamily: Fontfamily.Neuropolitical,
+        fontSize: size.xlg,
+        fontWeight: 'bold'
     }
 })
